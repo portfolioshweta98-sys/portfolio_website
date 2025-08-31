8d874e684fbaf4adb9f0b711c584042981165403
saqibullah4116 <saqibullah4116@gmail.com>
2025-08-31 09:10:37 +0500

    fixing

--- components/ui/timeline.tsx
+++ components/ui/timeline.tsx
@@ -32,14 +32,6 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
       className="w-full bg-white dark:bg-black-100 font-sans md:px-10 mt-10"
       ref={containerRef}
     >
-      <div className="max-w-7xl mx-auto px-4 md:px-8 lg:px-10">
-        <h2 className="text-lg md:text-4xl  text-black dark:text-white max-w-4xl">
-          Education and Experience.
-        </h2>
-        <p className="text-neutral-700 dark:text-neutral-300 text-sm md:text-base max-w-sm">
-          What I&apos;ve done so far
-        </p>
-      </div>
 
       <div ref={ref} className="relative max-w-7xl mx-auto pb-20">
         {data.map((item, index) => (
